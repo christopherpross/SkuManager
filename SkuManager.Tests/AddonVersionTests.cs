2ad90c7bdde488aced070188de36a5cdcf34fc42
Christopher Proß <cpross@mailbox.org>
2023-06-05 08:33:10 +0200

    add class for addonversions and logic to compare them

---
+++ SkuManager.Tests/AddonVersionTests.cs
@@ -0,0 +1,138 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace SkuManager.Tests;
+
+public class AddonVersionTests
+{
+    [Theory]
+    [InlineData("r34.17", 'r', 34, 17, 0)]
+    [InlineData("r34.16", 'r', 34, 16, 0)]
+    [InlineData("r34", 'r', 34, 0, 0)]
+    [InlineData("r31.8", 'r', 31, 8, 0)]
+    [InlineData("34.10", null, 34, 10, 0)]
+    [InlineData("1.0.0", null, 1, 0, 0)]
+    [InlineData("1", null, 1, 0, 0)]
+    public void Parse_ValidVersionString_ReturnsExpectedVersion(string versionString, char? expectedPrefix, int expectedMajor, int expectedMinor, int expectedPatch)
+    {
+        // Act
+        AddonVersion version = AddonVersion.Parse(versionString);
+
+        // Assert
+        version.Prefix.Should().Be(expectedPrefix);
+        version.Major.Should().Be(expectedMajor);
+        version.Minor.Should().Be(expectedMinor);
+        version.Patch.Should().Be(expectedPatch);
+    }
+
+    [Theory]
+    [InlineData("invalid")]
+    [InlineData("")]
+    [InlineData("r34.17.25.12")]
+    public void Parse_InvalidVersionString_ThrowsArgumentException(string versionString)
+    {
+        // Arrange & Act & Assert
+        versionString.Invoking(vs => AddonVersion.Parse(vs)).Should().Throw<ArgumentException>();
+    }
+
+    [Theory]
+    [InlineData("r34.17", "r34.16", false)]
+    [InlineData("r34.16", "r34.17", false)]
+    [InlineData("r34.17", "r34.17", true)]
+    [InlineData("r34", "r31.8", false)]
+    [InlineData("r34", "r35", false)]
+    [InlineData("34.10", "1.0.0", false)]
+    [InlineData("1.0.0", "1", true)]
+    [InlineData("1.0.0", "1.0.0", true)]
+    public void ComparisonEqual_Operators_CompareVersionsCorrectly(string versionString1, string versionString2, bool expectedComparisonResult)
+    {
+        // Arrange
+        AddonVersion version1 = AddonVersion.Parse(versionString1);
+        AddonVersion version2 = AddonVersion.Parse(versionString2);
+
+        // Act & Assert
+        (version1 == version2).Should().Be(expectedComparisonResult);
+        (version1 != version2).Should().Be(!expectedComparisonResult);
+    }
+
+    [Theory]
+    [InlineData("r34.17", "r34.16", false)]
+    [InlineData("r34.16", "r34.17", true)]
+    [InlineData("r34.17", "r34.17", false)]
+    [InlineData("r34", "r31.8", false)]
+    [InlineData("r34", "r35", true)]
+    [InlineData("34.10", "1.0.0", false)]
+    [InlineData("1.0.0", "1", false)]
+    [InlineData("1.0.0", "1.0.0", false)]
+    public void ComparisonLess_Operators_CompareVersionsCorrectly(string versionString1, string versionString2, bool expectedComparisonResult)
+    {
+        // Arrange
+        AddonVersion version1 = AddonVersion.Parse(versionString1);
+        AddonVersion version2 = AddonVersion.Parse(versionString2);
+
+        // Act & Assert
+        (version1 < version2).Should().Be(expectedComparisonResult);
+    }
+
+    [Theory]
+    [InlineData("r34.17", "r34.16", false)]
+    [InlineData("r34.16", "r34.17", true)]
+    [InlineData("r34.17", "r34.17", true)]
+    [InlineData("r34", "r31.8", false)]
+    [InlineData("r34", "r35", true)]
+    [InlineData("34.10", "1.0.0", false)]
+    [InlineData("1.0.0", "1", true)]
+    [InlineData("1.0.0", "1.0.0", true)]
+    public void ComparisonLessOrEqual_Operators_CompareVersionsCorrectly(string versionString1, string versionString2, bool expectedComparisonResult)
+    {
+        // Arrange
+        AddonVersion version1 = AddonVersion.Parse(versionString1);
+        AddonVersion version2 = AddonVersion.Parse(versionString2);
+
+        // Act & Assert
+        (version1 <= version2).Should().Be(expectedComparisonResult);
+    }
+
+    [Theory]
+    [InlineData("r34.17", "r34.16", true)]
+    [InlineData("r34.16", "r34.17", false)]
+    [InlineData("r34.17", "r34.17", false)]
+    [InlineData("r34", "r31.8", true)]
+    [InlineData("r34", "r35", false)]
+    [InlineData("34.10", "1.0.0", true)]
+    [InlineData("1.0.0", "1", false)]
+    [InlineData("1.0.0", "1.0.0", false)]
+    public void ComparisonGreater_Operators_CompareVersionsCorrectly(string versionString1, string versionString2, bool expectedComparisonResult)
+    {
+        // Arrange
+        AddonVersion version1 = AddonVersion.Parse(versionString1);
+        AddonVersion version2 = AddonVersion.Parse(versionString2);
+
+        // Act & Assert
+        (version1 > version2).Should().Be(expectedComparisonResult);
+    }
+
+    [Theory]
+    [InlineData("r34.17", "r34.16", true)]
+    [InlineData("r34.16", "r34.17", false)]
+    [InlineData("r34.17", "r34.17", true)]
+    [InlineData("r34", "r31.8", true)]
+    [InlineData("r34", "r35", false)]
+    [InlineData("34.10", "1.0.0", true)]
+    [InlineData("1.0.0", "1", true)]
+    [InlineData("1.0.0", "1.0.0", true)]
+    public void ComparisonGreaterOrEqual_Operators_CompareVersionsCorrectly(string versionString1, string versionString2, bool expectedComparisonResult)
+    {
+        // Arrange
+        AddonVersion version1 = AddonVersion.Parse(versionString1);
+        AddonVersion version2 = AddonVersion.Parse(versionString2);
+
+        // Act & Assert
+        (version1 >= version2).Should().Be(expectedComparisonResult);
+    }
+
+
+
+}
+
---
+++ SkuManager/AddonVersion.cs
@@ -0,0 +1,154 @@
+﻿using Ardalis.GuardClauses;
+
+namespace SkuManager;
+public class AddonVersion : IComparable<AddonVersion>
+{
+    public char? Prefix { get; set; }
+    public int Major { get; }
+    public int Minor { get; }
+    public int Patch { get; }
+
+    public AddonVersion(int major, int minor = 0, int patch = 0, char? prefix = null)
+    {
+        Major = major;
+        Minor = minor;
+        Patch = patch;
+        Prefix = prefix;
+    }
+
+    public static AddonVersion Parse(string versionString)
+    {
+        if (string.IsNullOrEmpty(versionString))
+            throw new ArgumentException("Invalid version string");
+
+        char? prefix = null;
+        int majorStartIndex = 0;
+
+        if (char.IsLetter(versionString[0]))
+        {
+            prefix = versionString[0];
+            majorStartIndex = 1;
+        }
+
+        string[] parts = versionString.Substring(majorStartIndex).Split('.');
+        if (parts.Length < 1 || parts.Length > 3)
+            throw new ArgumentException("Invalid version string");
+
+        if (!int.TryParse(parts[0], out int major))
+            throw new ArgumentException("Invalid version string");
+
+        int minor = 0;
+        int patch = 0;
+
+        if (parts.Length >= 2)
+        {
+            if (!int.TryParse(parts[1], out minor))
+                throw new ArgumentException("Invalid version string");
+
+            if (parts.Length == 3)
+            {
+                if (!int.TryParse(parts[2], out patch))
+                    throw new ArgumentException("Invalid version string");
+            }
+        }
+
+        return new AddonVersion(major, minor, patch, prefix);
+    }
+
+    public int CompareTo(AddonVersion? other)
+    {
+        Guard.Against.Null(other, nameof(other));
+
+        if (Major != other.Major)
+            return Major.CompareTo(other.Major);
+
+        if (Minor != other.Minor)
+            return Minor.CompareTo(other.Minor);
+
+        return Patch.CompareTo(other.Patch);
+    }
+
+    public static bool operator <(AddonVersion left, AddonVersion right)
+    {
+        if (left is null)
+            return false;
+
+        return left.CompareTo(right) < 0;
+    }
+
+    public static bool operator >(AddonVersion left, AddonVersion right)
+    {
+        if (left is null)
+            return false;
+
+        return left.CompareTo(right) > 0;
+    }
+
+
+    public static bool operator <=(AddonVersion left, AddonVersion right)
+    {
+        if (left is null)
+            return false;
+
+        return left.CompareTo(right) <= 0;
+    }
+
+    public static bool operator >=(AddonVersion left, AddonVersion right)
+    {
+        if (left is null)
+            return false;
+
+        return left.CompareTo(right) >= 0;
+    }
+
+    public static bool operator ==(AddonVersion left, AddonVersion right)
+    {
+        if (left is null)
+            return right is null;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(AddonVersion left, AddonVersion right)
+    {
+        return !(left == right);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is AddonVersion other)
+        {
+            return Major == other.Major && Minor == other.Minor && Patch == other.Patch;
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            int hash = 17;
+            hash = hash * 23 + Major.GetHashCode();
+            hash = hash * 23 + Minor.GetHashCode();
+            hash = hash * 23 + Patch.GetHashCode();
+            return hash;
+        }
+    }
+
+    public override string ToString()
+    {
+        if (Minor == 0 && Patch == 0)
+        {
+            return $"{((Prefix is null) ? string.Empty : Prefix)}{Major}";
+        }
+        else if (Patch == 0)
+        {
+            return $"{((Prefix is null) ? string.Empty : Prefix)}{Major}.{Minor}";
+        }
+        else
+        {
+            return $"{((Prefix is null) ? string.Empty : Prefix)}{Major}.{Minor}.{Patch}";
+        }
+    }
+}
